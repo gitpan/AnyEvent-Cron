096b1de2d4a0c6bd2d8402ef62ea259655f0290d
Yo-An Lin (林佑安) <cornelius@cpan.org>
2011-11-14 18:22:29 +0000

    Import of CORNELIUS/AnyEvent-Cron-0.03 from CPAN.
    gitpan-cpan-distribution: AnyEvent-Cron
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CORNELIUS/AnyEvent-Cron-0.03.tar.gz
    gitpan-cpan-author:       CORNELIUS
    gitpan-cpan-maturity:     released

--- lib/AnyEvent/Cron.pm
+++ lib/AnyEvent/Cron.pm
@@ -3,18 +3,25 @@ use warnings;
 use strict;
 use DateTime;
 use AnyEvent;
-use Any::Moose;
+use Moose;
+use Try::Tiny;
+use DateTime::Event::Cron;
+use v5.12;
 
-our $VERSION = '0.021';
+our $VERSION = '0.03';
 
-has after =>
-    ( is => 'rw' , isa => 'Int' , default => sub { 0 } );
+
+# map of expiration formats to their respective time in seconds
+my %_Expiration_Units = ( map(($_,             1), qw(s second seconds sec)),
+                          map(($_,            60), qw(m minute minutes min)),
+                          map(($_,         60*60), qw(h hour hours)),
+                          map(($_,      60*60*24), qw(d day days)),
+                          map(($_,    60*60*24*7), qw(w week weeks)),
+                          map(($_,   60*60*24*30), qw(M month months)),
+                          map(($_,  60*60*24*365), qw(y year years)) );
 
 has interval => 
     ( is => 'rw' , isa => 'Int' , default => sub { 1 } );
-
-has events => 
-    ( is =>  'rw' , isa => 'ArrayRef' , default => sub { [  ] } );
 
 has verbose => 
     ( is => 'rw' , isa => 'Bool' , default => sub { 0 } );
@@ -26,107 +33,62 @@ has debug =>
 has ignore_floating =>
     ( is => 'rw',  isa => 'Bool' , default => sub { 0 } );
 
+has jobs =>
+    traits  => ['Array'],
+    handles => {
+        add_job => 'push'
+    },
+    is => 'rw', 
+    isa => 'ArrayRef' ,
+    default => sub {  [ ] }
+
+    ;
+
+
+has timers => ( is => 'rw', isa => 'ArrayRef' , default => sub { [ ] } );
+
 use Scalar::Util qw(refaddr);
-
-sub create_interval_event {
-    my $self = shift;
-    my $ref  = shift;
-    $ref = {
-        %$ref,
-        type      => 'interval',
-        triggered => 0,
-        };
-    $ref->{name} ||= 'interval-' . refaddr($ref);
-    print "hook interval on: "
-            , join( ':', grep {$_} map { $ref->{$_} } qw(hour minute second) ) 
-            , "\n" 
-            if $self->verbose;
-    push @{ $self->events }, $ref;
-}
-
-sub delete {
-    my $self = shift;
-    my $name = shift;
-    my @events = @{ $self->events };
-    $self->events( [ grep { $_->{name} ne $name } @{ $self->events } ] );
-}
-
-sub create_datatime_event {
-    my ( $self, $dt, $cb ) = @_;
-    push @{ $self->events }, {
-        type      => 'datetime',
-        triggered => 0,
-        datetime  => $dt,
-        callback  => $cb,
-        name      => 'datetime-'. refaddr($cb),
-    };
-}
 
 sub add {
     my $self = shift;
-    if ( !ref( $_[0] ) && ref( $_[1] ) eq 'CODE' ) {
-        my ( $ts_string, $cb ) = @_;
+    my ( $timespec, $cb , %args ) = @_;
 
-        # TODO: support more format
-
-        # hour:minute
-        if ( $ts_string =~ m{^(\d+):(\d+)$} ) {
-            my ( $hour, $minute ) = ( $1, $2 );
-            $self->create_interval_event( {
-                    hour     => $hour,
-                    minute   => $minute,
-                    callback => $cb,
-            } );
+    # try to create with crontab format
+    try {
+        my $cron_event = DateTime::Event::Cron->new($timespec);
+        $self->add_job({
+            event => $cron_event,
+            cb => $cb,
+            %args
+        });
+    } 
+    catch {
+        given ( $timespec ) {
+            # hour:minute per day
+            when( m{^(\d+):(\d+)$} ) {
+                my ( $hour, $minute ) = ( $1, $2 );
+                $self->add_job({
+                    time => { hour => $hour, minute => $minute },
+                    cb => $cb,
+                    %args,
+                });
+            }
+            when( m{^\s*(\d+)\s*(\w+)} ) {
+                my ( $number, $unit ) = ( $1, $2 );
+                my $seconds = $number * $_Expiration_Units{$unit};
+                $self->add_job({
+                    seconds => $seconds,
+                    cb      => $cb,
+                    %args
+                });
+                # $self->create_interval_event( { second => $seconds, callback => $cb } );
+            }
+            default {
+                die 'time string format is not supported.';
+            }
         }
-        elsif( $ts_string =~ m{^(\d+):(\d+):(\d+)$} ) {
-            my ( $hour, $minute ,$second ) = ( $1, $2 , $3 );
-            print "hook interval on: $hour $minute $second" if $self->verbose;
-            $self->create_interval_event( {
-                    hour     => $hour,
-                    minute   => $minute,
-                    second   => $second,
-                    callback => $cb,
-            });
-        }
-        elsif( $ts_string =~ m{(\d+):\*:\*} ) {
-            my $hour = $1;
-            $self->create_interval_event( { hour => $hour, callback => $cb } );
-        }
-        elsif( $ts_string =~ m{\*:(\d+):\*} ) {
-            my $minute = $1;
-            $self->create_interval_event( { minute => $minute, callback => $cb } );
-        }
-        elsif( $ts_string =~ m{\*:\*:(\d+)} ) {
-            my $second = $1;
-            $self->create_interval_event( { second => $second, callback => $cb } );
-        }
-        else {
-            warn 'time string format is not supported.';
-            return;
-        }
-    }
-    elsif( ref($_[0]) eq 'DateTime' && ref($_[1]) eq 'CODE' ) {
-        my ($dt,$cb) = @_;
-        $self->create_datatime_event( $dt, $cb );
-    }
-    else {
-        my @es = @_;
-        map { $_->{triggered} = 0 } @es;
-        push @{ $self->events }, @es;
-    }
+    };
     return $self;
-}
-
-
-
-sub _check_interval {
-    my ( $self, $e, $dt ) = @_;
-    my @keys = qw(hour minute second);
-    my $ret  = 1;
-
-    # find all defined columns and compare them with anyevent datetime object.
-    map { $e->{$_} != $dt->$_ ? $ret = 0 : undef } grep { defined $e->{$_} } @keys;
-    return $ret;
 }
 
 sub _call_event {
@@ -138,53 +100,86 @@ sub _call_event {
     }
 }
 
-sub run {
-    my $self = shift;
-    $self->{w} = AnyEvent->timer (
-        after    => $self->after,
-        interval => $self->interval,
-        cb       => sub {
-            # check time
-            my $now = AnyEvent->now;
-            my $dt = DateTime->from_epoch( epoch => $now );
-            print $dt , "\n" if $self->verbose;
-            for my $e ( @{ $self->events } ) {
-                if( $e->{type} eq 'interval' ) {
-                    my $match = $self->_check_interval( $e , $dt );
-                    if( $match ) {
-                        $self->_call_event( $e , $dt );
-                    }
-                    else {
-                        # reset triggered flag
-                        $e->{triggered} = 0 if $e->{triggered} ;
-                    }
-                }
-                elsif( $e->{type} eq 'datetime' ) {
-                    $self->_check_datetime( $e, $dt );
-                }
+sub _schedule {
+    my ($self,$job) = @_;
+
+    AnyEvent->now_update();
+    my $now_epoch = AnyEvent->now;
+    my $next_epoch;
+    my $delay;
+    my $name = $job->{name};
+    my $debug = $job->{debug};
+
+    if( $job->{event} ) {
+        my $event = $job->{event};
+        $next_epoch = $event->next->epoch;  # set next schedule time
+        $delay      = $next_epoch - $now_epoch;
+        warn "delay:",$delay if $debug;
+    } 
+    elsif( $job->{seconds} ) {
+        $next_epoch = $now_epoch + $job->{seconds};
+        $delay      = $next_epoch - $now_epoch;
+        warn "delay:",$delay if $debug;
+    }
+    elsif( $job->{time} ) {
+        my $time = $job->{time};
+        my $now = DateTime->from_epoch( epoch => $now_epoch ); # depends on now
+        my $next = $now->clone;
+        $next->set( %$time );
+
+        # now > the scheduled time
+        if( DateTime->compare( $now, $next ) == 1 ) {
+            if( $time->{month} ) {
+                $next->add( years => 1 );
             }
+            elsif( $time->{day} ) {
+                $next->add( months => 1 );
+            }
+            elsif( $time->{hour} ) {
+                $next->add( days => 1 );
+            }
+            elsif( $time->{minute} ) {
+                $next->add( hours => 1 );
+            }
+            elsif( $time->{second} ) {
+                $next->add( minutes => 1 );
+            }
+            else {
+                die 'unknown spec';
+            }
+        }
+    }
+
+    $job->{next}{ $next_epoch } = 1;
+    $job->{watchers}{$next_epoch} = AnyEvent->timer(
+        after    => $delay,
+        cb       => sub {
+            $self->{_cv}->begin;
+            delete $job->{watchers}{$next_epoch};
+
+            $self->_schedule($job) unless $job->{once};
+
+            if ( $job->{single} && $job->{running}++ ) {
+                print STDERR "Skipping job '$name' - still running\n"
+                    if $debug;
+            }
+            else {
+                eval { $job->{cb}->( $self->{_cv}, $job ); 1 }
+                    or warn $@ || 'Unknown error';
+                delete $job->{running};
+                print STDERR "Finished job '$name'\n"
+                    if $debug;
+            }
+            $self->{_cv}->end;
         }
     );
 }
 
-sub _check_datetime {
-    my ($self,$e,$dt) = @_;
-    if( $self->ignore_floating ) { 
-        # ignore floating
-        if( ! $e->{triggered} 
-                && $dt->month == $e->{datetime}->month
-                && $dt->day == $e->{datetime}->day
-                && $dt->hour == $e->{datetime}->hour
-                && $dt->minute == $e->{datetime}->minute
-                && $dt->second == $e->{datetime}->second
-            ) {
-            $self->_call_event( $e , $dt );
-        }
-    }
-    else {
-        if( ! $e->{triggered} && $dt == $e->{datetime} ) {
-            $self->_call_event( $e , $dt );
-        }
+sub run {
+    my $self = shift;
+    my $cv = $self->{_cv} = AnyEvent->condvar;
+    for my $job ( @{ $self->jobs } ) {
+        $self->_schedule($job);
     }
 }
 
@@ -201,81 +196,25 @@ AnyEvent::Cron - Crontab in AnyEvent! provide an interface to register event on 
     my $cron = AnyEvent::Cron->new( 
             verbose => 1,
             debug => 1,
-            after => 1,
-            interval => 1,
             ignore_floating => 1
     );
 
-    # 00:00 (hour:minute)
+                # 00:00 (hour:minute)
     $cron->add("00:00" => sub { warn "zero"; })
-
-        # hour : minute : second 
-        ->add( "*:*:10" => sub { })
-        ->add( "1:*:*" => sub { })
-
-        ->add( DateTime->now => sub { warn "datetime now" } )
+        ->add( '* * * * *' => sub {  } )
+        ->add( '1 seconds' => sub {  } )
+        ->add( '3 days' => sub {  } )
         ->run();
 
     my $cv = AnyEvent->condvar;
     $cv->recv;
 
-Or:
-
-    $cron->add({  
-        type => 'interval',
-        second => 0 ,
-        callback => sub { 
-            warn "SECOND INTERVAL TRIGGERD";
-        },
-    },{  
-        type => 'interval',
-        hour => DateTime->now->hour , 
-        minute =>  DateTime->now->minute ,
-        callback => sub { 
-            warn "HOUR+MINUTE INTERVAL TRIGGERD";
-        },
-    },{  
-        type => 'interval',
-        hour => DateTime->now->hour ,
-        callback => sub { 
-            warn "HOUR INTERVAL TRIGGERD";
-        },
-    },{  
-        type => 'interval',
-        minute => DateTime->now->minute ,
-        callback => sub { 
-            warn "MINUTE INTERVAL TRIGGERD";
-        },
-    },{
-        type => 'datetime' ,
-        callback => sub { warn "DATETIME TRIGGED"  },
-        datetime => (sub { 
-                # my $dt = DateTime->now->add_duration( DateTime::Duration->new( minutes => 0 ) );
-                my $dt = DateTime->now;
-                # $dt->set_second(0);
-                # $dt->set_nanosecond(0);
-                warn "Next trigger: ", $dt;
-                return $dt; })->()
-    })->run();
-
 
 =head1 METHODS
-
-=head2 add( @events )
 
 =head2 add( "12:36" => sub {     } )
 
 =head2 add( DateTime->now => sub {     } )
-
-=head2 create_interval_event
-
-    $cron->create_interval_event({
-        hour => $hour,
-        minute => $minute,
-        callback => $cb,
-    });
-
-=head2 create_datatime_event
 
 =head1 AUTHOR
 
@@ -286,9 +225,6 @@ Cornelius, C<< <cornelius.howl_at_gmail.com> >>
 Please report any bugs or feature requests to C<bug-anyevent-cron at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=AnyEvent-Cron>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
